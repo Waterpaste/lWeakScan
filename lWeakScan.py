b33ed4e9a10d0f613268588cb154915edc06b410
luoshu <1737257007@qq.com>
2018-08-22 09:51:06 +0800

    File target scan waits to be modified to progress bar, scan success target increases time

--- lWeakScan.py
+++ lWeakScan.py
@@ -10,13 +10,16 @@ import json
 import threading
 import lib 
 import requests
+import time
+from progressbar import *
 from queue import Queue
 sys.path.append("lib")
+
 
 #单个ip/网段扫描
 def ipscan(host,p):
     nm = nmap.PortScanner()
-    print("Waiting >>>>>>>>>>>>>>>>")
+    print("Please be patient, the scan time may be too long ......")
     nm.scan(hosts=host, ports=p, arguments='-sV')
     print(nm.command_line())
     for survive in nm.all_hosts():
@@ -27,13 +30,30 @@ def ipscan(host,p):
                 if nm[survive].tcp(port)["product"] == '':
                     continue
                 else:
-                    judgment(host,port,nm[survive].tcp(port)["product"])
+                    judgment(survive,port,nm[survive].tcp(port)["product"])
+
+
+#file目标扫描
+def portinfos(p,quantity):
+    while True:
+        try:
+            host = queue.get(timeout=0.1)
+            global num
+            nma = nmap.PortScannerAsync()
+            apple = nma.scan(hosts=host, arguments='-sV -p '+p,callback=callback_result)
+            while nma.still_scanning():
+                nma.wait(100)
+        except Exception as e:
+            break
+        queue.task_done()
+        progress(quantity)
+
 
 
 def callback_result(host, scan_data):
-    print('------------------')
+    #print('------------------')
     if not scan_data['scan']:
-        print(host+' : Port not scanned...')
+        pass
     else:
         for x in scan_data['scan'][host]['tcp']:
             if(scan_data['scan'][host]['tcp'][x]['product'] == ''):
@@ -54,29 +74,37 @@ def judgment(host,port,values):
                 if 'jboss' in re_html:
                     x = 'jboss'
             imp = __import__(x)
-            print(host+': Scanning  '+x+'...')
             con = imp.WeakScan(host,port)
-            fp = open("suc_host.txt",'a')
             if not con:
-                print(host+' failure')
                 continue
             else:
-                print('%s:%s is %s  success %s ' % (host,port,x,con))
-                fp.write(host+':'+str(port)+' success: '+con+'\n')
-            fp.close()
-            
+                #lock.acquire()
+                # global num               
+                # num += 1
+                fp = open("suc_host.txt",'a')
+                #print('%s:%s is %s  success %s ' % (host,port,x,con))
+                localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                fp.write(str(localtime)+' '+host+':'+str(port)+' success: '+con+'\n')
+                fp.close()
+                #lock.release()
 
-def portinfos(p):
-    while True:
-        try:
-            host = queue.get(timeout=0.1)
-            nma = nmap.PortScannerAsync()
-            nma.scan(hosts=host, arguments='-sV -p '+p,callback=callback_result)
-            while nma.still_scanning():
-                print("Waiting >>> %s" % host)
-                nma.wait(100)
-        except Exception as e:
-            break
+
+def progress(quantity):
+    with lock:       
+        pbar.maxval = quantity
+        finished = pbar.maxval - queue.unfinished_tasks
+        pbar.update(finished)
+        time.sleep(0.1)
+
+#f 参数处理
+def filetar(file,p):
+    file = args.f
+    p = args.p
+    gethost(file)
+    threads = [threading.Thread(target=portinfos, args=(p,queue.unfinished_tasks,)) for i in range(int(threadnum))]
+    list(map(lambda x:x.start() ,threads))
+    list(map(lambda x:x.join() ,threads))
+    
 
 def gethost(file):
     for x in open(file).readlines():
@@ -85,6 +113,7 @@ def gethost(file):
             continue
         for ip in file.split():
             queue.put(ip)
+
 
 def args():
     arg = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,description='Weak password scanner.(https://www.gitshell.org)',usage='scan.py [options]')
@@ -100,15 +129,17 @@ if __name__ == '__main__':
         sys.argv.append('--help')
     args = args()
     threadnum = args.t
+    #num = 0
     if args.f:
-        file = args.f
-        p = args.p
         queue = Queue()
-        gethost(file)
-        threads = [threading.Thread(target=portinfos, args=(p,)) for i in range(int(threadnum))]
-        list(map(lambda x:x.start() ,threads))
-        list(map(lambda x:x.join() ,threads))
+        lock = threading.Lock()
+        widgets = ['Scanning:', Percentage(), ' ',Bar(marker=RotatingMarker()),' ', Timer(), ' ', ' ']
+        pbar = ProgressBar(widgets=widgets, maxval=1).start()
+        filetar(args.f,args.p)
+        pbar.finish()
+        #print(num)
     elif args.i:
         ipscan(args.i,args.p)
-
+    print('==='*30)
+    print('Scan is complete, please check the suc_host.txt file.') 
 
